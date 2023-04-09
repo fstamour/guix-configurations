fdc1c89c9a7786de96305c21edbb6709f9a7cf3e
Francis St-Amour <fr.stamour@gmail.com>
2023-04-09 00:35:55 -0400

    fix some indentations

--- home/home-configuration.scm
+++ home/home-configuration.scm
@@ -23,11 +23,11 @@
 
 (define %environment-variables
   (simple-service 'some-useful-env-vars-service
-		  home-environment-variables-service-type
-		  `(("EDITOR" . "emacsclient -nw -a emacs -nw")
-		    ;; Meh
-		    ;; ("VISUAL" . "emacsclient -a emacs")
-		    )))
+                  home-environment-variables-service-type
+                  `(("EDITOR" . "emacsclient -nw -a emacs -nw")
+                    ;; Meh
+                    ;; ("VISUAL" . "emacsclient -a emacs")
+                    )))
 
 ;; Putting those in a variable because I'm using bash for now, but
 ;; I'll use fish in the future. I would be nice to have those aliases
@@ -55,22 +55,22 @@
   (simple-service 'syncthing home-shepherd-service-type
                   (list (shepherd-service
                          (provision '(syncthing))
-			 (documentation "Run syncthing as a shepherd (user) service")
-			 (start
-			  #~(make-forkexec-constructor
-			     (list
-			      #$(file-append syncthing "/bin/syncthing")
-			      ;; TODO Put synchting's log in its own file
-			      ;; -logfile=...
-			      ;; -logflag=... To specify the format?
+                         (documentation "Run syncthing as a shepherd (user) service")
+                         (start
+                          #~(make-forkexec-constructor
+                             (list
+                              #$(file-append syncthing "/bin/syncthing")
+                              ;; TODO Put synchting's log in its own file
+                              ;; -logfile=...
+                              ;; -logflag=... To specify the format?
                               "-no-browser")))
-			 (stop #~(make-kill-destructor))))))
+                         (stop #~(make-kill-destructor))))))
 
 (define %my-poor-eyes-i-cant-adjust-my-backlight-because-i-didnt-install-the-right-drivers-yet
   (service home-redshift-service-type
            (home-redshift-configuration
             (location-provider 'manual)
-	    ;; Took Ottawa's coordinates
+            ;; Took Ottawa's coordinates
             (latitude 45.4215)
             (longitude -75.6972))))
 
@@ -83,33 +83,33 @@
   (service home-xmodmap-service-type
            (home-xmodmap-configuration
             (key-map '(;; Switch ( with [ and ) with ]
-		       ("keycode 18" . "9 braceleft")
-		       ("keycode 19" . "0 braceright")
-		       ("keycode 34" . "parenleft bracketleft")
-		       ("keycode 35" . "parenright bracketright")
-		       ;; Use Caps Lock as a compose key
-		       ("keysym Caps_Lock" . "Multi_key Caps_Lock")
-		       "clear Lock")))))
+                       ("keycode 18" . "9 braceleft")
+                       ("keycode 19" . "0 braceright")
+                       ("keycode 34" . "parenleft bracketleft")
+                       ("keycode 35" . "parenright bracketright")
+                       ;; Use Caps Lock as a compose key
+                       ("keysym Caps_Lock" . "Multi_key Caps_Lock")
+                       "clear Lock")))))
 
 (define %ssh-agent
   (service home-ssh-agent-service-type
          (home-ssh-agent-configuration
           ;; TODO maybe another time... (extra-options '("-t" "1h30m"))
-	  )))
+          )))
 
 (define %files
   (service home-files-service-type
-	   `((".xsession" ,(local-file "xsession")))))
+           `((".xsession" ,(local-file "xsession")))))
 
 (home-environment
  ;; Below is the list of packages that will show up in your
  ;; Home profile, under ~/.guix-home/profile.
  (packages (specifications->packages
-	    (list
+            (list
              "fd"
-	     "make"
+             "make"
 
-	     "podman"
+             "podman"
 
              "git"
              "tmux"
@@ -117,26 +117,26 @@
              "readline"
              "sqlite"
 
-	     "sbcl-slynk"
+             "sbcl-slynk"
              "sbcl"
              "sbcl-cl+ssl"
-	     "stumpwm-with-slynk" ;; TODO install as root or user??
+             "stumpwm-with-slynk" ;; TODO install as root or user??
 
              "emacs-next" ;; for emacs 29.0
-	     "emacs-magit"
-	     "emacs-guix"
-	     "emacs-paredit"
+             "emacs-magit"
+             "emacs-guix"
+             "emacs-paredit"
              "emacs-vertico"
              "emacs-darkroom"
              "emacs-focus"
              "emacs-sly"
 
-	     "gforth"
+             "gforth"
 
-	     "w3m"
+             "w3m"
 
-	     "icecat"
-	     "kitty"
+             "icecat"
+             "kitty"
              "firefox" ;; nonguix
              "freecad"
              "keepassxc"
@@ -144,8 +144,8 @@
              "steam-nvidia" ;; nonguix
              "xbacklight"
              "xclip"
-	     "xmodmap" "setxkbmap"
-	     )))
+             "xmodmap" "setxkbmap"
+             )))
 
  ;; Below is the list of Home services.  To search for available
  ;; services, run 'guix home search KEYWORD' in a terminal.
