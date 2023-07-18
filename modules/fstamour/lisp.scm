ae7ef6a075ffb0c9493ddda11ed507a5e582f8df
Francis St-Amour <fr.stamour@gmail.com>
2023-07-17 23:16:28 -0400

    add package definition sbcl-breeze

--- modules/fstamour/lisp.scm
+++ modules/fstamour/lisp.scm
@@ -20,6 +20,63 @@
 ;; TODO other implementations (e.g. ecl) see see asdf-build-system/ecl
 ;; TODO my fork of stumpwm
 
+;;; tips:
+;;; guix hash -rx . on the target's repo
+;;; https://github.com/drewc/guix/blob/master/guix/licenses.scm
+;;; on guix, asdf is patched to search for configurations files in xdg-config-dirs and? xdg-home
+
+(define-public sbcl-breeze
+  (let ((commit "e76ce087980fdb30a4124c77177d06748ad5c73e")
+        (repo-url "https://github.com/fstamour/breeze"))
+    (package
+     (name "sbcl-breeze")
+     (version "0")
+     ;; (source (origin
+     ;;          (method git-fetch)
+     ;;          (uri (git-reference
+     ;;                (url repo-url)
+     ;;                (commit commit)))
+     ;;          (file-name (git-file-name name version))
+     ;;          (sha256 (base32 "0rq57ccbbxp48g37xrpcqj07asxrl1xd3iql49r0msqjhhys6n8n"))))
+     (source (local-file "/home/fstamour/quicklisp/local-projects/breeze"
+                         #:recursive? #t))
+     (build-system asdf-build-system/sbcl)
+     (inputs (list
+              sbcl-3bmd
+              sbcl-spinneret
+              sbcl-alexandria
+              sbcl-anaphora
+              sbcl-bordeaux-threads
+              sbcl-chanl
+              ;; sbcl-cl-hash-util
+              sbcl-cl-ppcre
+              sbcl-closer-mop
+              sbcl-eclector
+              sbcl-quickproject
+              sbcl-cl-str
+              sbcl-trivial-features
+              sbcl-trivial-package-local-nicknames
+              sbcl-trivial-timeout
+              ))
+     ;; (native-inputs (list sbcl-parachute))
+     ;; Propagated inputs: Installed in the store and in the profile,
+     ;; as well as being present at build time.
+     (propagated-inputs
+      (list
+       sbcl-parachute))
+     (arguments
+      (list #:phases
+            #~(modify-phases %standard-phases
+                             (add-after 'unpack 'disable-failing-tests
+                                        (lambda _
+                                          (substitute* "breeze.asd"
+                                                       ;; Guix does not have Quicklisp, and probably never will.
+                                                       (("\\(:file \"quicklisp\"\\)") "")))))))
+     (home-page repo-url)
+     (synopsis "Experiments on workflow with common lisp.")
+     (description "Experiments on workflow with common lisp.")
+     (license license:bsd-2))))
+
 (define-public sbcl-simpbin
   (let ((commit "6f9f1c196ca8f363b478bab0a8623f53b89e5586")
         (repo-url "https://github.com/fstamour/simpbin"))
@@ -40,6 +97,15 @@
      (synopsis "A common lisp library to store data in a simple binary format")
      (description "")
      (license license:gpl3))))
+
+;; sbcl-package->cl-source-package is supposed to be exported by (guix
+;; build-system asdf), but...
+;;
+;; (define-public cl-simpbin
+;;   (sbcl-package->cl-source-package sbcl-simpbin))
+
+;; (define-public ecl-simpbin
+;;   (sbcl-package->ecl-source-package sbcl-simpbin))
 
 ;; TODO add
 ;; https://github.com/compufox/with-user-abort
