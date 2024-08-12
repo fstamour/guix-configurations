3c6ef92bb30a9c835f2b297a6962689320ec3240
Francis St-Amour <fr.stamour@gmail.com>
2024-08-11 21:14:14 -0400

    bump channels

--- channels.scm
+++ channels.scm
@@ -1,24 +1,24 @@
 (list (channel
-        (name 'guix)
-        (url "https://git.savannah.gnu.org/git/guix.git")
-        (branch "master")
-        (commit
-          "d6a6e832e6e318eec0c4866fdde62ae47f29defa")
-        (introduction
-          (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
-            (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
-      (channel
         (name 'nonguix)
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "0018464a1f55586654d5a51b6ceb7a6e3c4fe2f3")
+          "7081518be7d2dbb58f3fbfeb1785254a6f0059c8")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
             (openpgp-fingerprint
-              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "aa9ac252206615713ab988d7068da9e14a9bccc0")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
 
 ;; warning: GUIX_PACKAGE_PATH="/home/fstamour/dev/guix-configurations/modules:/home/fstamour/dev/guix-configurations/modules"
